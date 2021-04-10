de99e9dbdc99520c6cbe87f09b03f459b442be2d
Kate <test@gmail.com>
2021-04-10 00:35:18 -0400

    delete not used files got from template

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,34 @@
 import { useAuth } from 'context/auth.context'
-import { Route } from 'react-router-dom'
+import { Redirect, Route, Switch } from 'react-router'
+import AppProviders from './context'
 import IndexScreen from 'screens'
 import CampgroundScreen from 'screens/campground'
 import CampgroundsScreen from 'screens/campgrounds'
-import UnAuthedRoutes from 'UnAuthedRoutes'
+import LoginScreen from 'screens/login'
+import RegisterScreen from 'screens/register'
+import * as React from 'react'
+
+function UnAuthedRoutes() {
+  const { user } = useAuth()
+  if (user) {
+    return <Route path="/*" component={() => <Redirect to="/" />} />
+  }
+  return (
+    <Switch>
+      <Route path="/login" exact component={LoginScreen} />
+      <Route path="/register" exact component={RegisterScreen} />
+      <Route path="*" component={() => <Redirect to="/" />} />
+    </Switch>
+  )
+}
 
 export default function App() {
-  const { user } = useAuth()
   return (
-    <>
+    <AppProviders>
       <Route path="/" exact component={IndexScreen} />
       <Route path="/campgrounds" exact component={CampgroundsScreen} />
       <Route path="/campgrounds/:id" exact component={CampgroundScreen} />
-      {user ? null : <UnAuthedRoutes />}
-    </>
+      <UnAuthedRoutes />
+    </AppProviders>
   )
 }
