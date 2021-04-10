017f4924a5fb4c0c959eb5123ab749d799055cc4
Kate <test@gmail.com>
2021-04-09 21:37:20 -0400

    remove log

--- backend/src/common/auth/strategies/local.strategy.ts
+++ backend/src/common/auth/strategies/local.strategy.ts
@@ -20,7 +20,6 @@ export class LocalStrategy extends PassportStrategy(Strategy) {
   // For each strategy, passport will call the verify function(validate)
   // For local strategy, passport expects a validate method with the following signature
   async validate(email: string, password: string): Promise<IUser> {
-    console.log(email, password);
     const user = await this.userService.findUser({ email, password });
     if (user) return this.authService.signUser(user);
     throw new UnauthorizedException('Wrong credentials provided');
