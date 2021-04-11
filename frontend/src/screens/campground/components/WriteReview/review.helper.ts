70a2b0a5efedd0af2c2077d97996955f8d3fec63
Kate <test@gmail.com>
2021-04-11 19:41:48 -0400

    min value for rating is 1

--- frontend/src/screens/campground/components/WriteReview/review.helper.ts
+++ frontend/src/screens/campground/components/WriteReview/review.helper.ts
@@ -4,11 +4,11 @@ export const ReviwSchema = Yup.object().shape({
   title: Yup.string().required(`Review title can't be empty.`),
   body: Yup.string().required(`Review content can't be empty.`),
   rating: Yup.number()
-    .min(0, 'Please add ratting.')
+    .min(1, 'Please add ratting.')
     .max(5, 'Maximum ratting value is five.')
     .required('Please add review ratting.'),
 })
 
 export function getIntialValues(campground: string) {
-  return { campground, title: '', body: '', rating: -1 }
+  return { campground, title: '', body: '', rating: 0 }
 }
