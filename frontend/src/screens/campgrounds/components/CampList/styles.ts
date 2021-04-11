466a4b5e29a5cf076bb04af8a7985f4703ead0df
Kate <test@gmail.com>
2021-04-10 23:40:42 -0400

    use shared ImgSwiper for campground item at campground list

--- frontend/src/screens/campgrounds/components/CampList/styles.ts
+++ frontend/src/screens/campgrounds/components/CampList/styles.ts
@@ -1,6 +1,5 @@
 import { Card, Content } from 'components/lib'
 import styled from 'styled-components'
-import 'swiper/swiper.scss'
 import { medium, xlarge } from 'styles/media-queries'
 
 export const CampgroundWrap = styled(Card).attrs(() => ({
@@ -59,27 +58,5 @@ export const Wrapper = styled(Content)`
   }
   .campground-item {
     margin-bottom: 2rem;
-  }
-`
-
-export const SwipperWrap = styled.div`
-  position: relative;
-  button {
-    position: absolute;
-    padding: 0.25rem 0;
-    background-color: rgba(0, 0, 0, 0.2);
-    top: 50%;
-    z-index: 2000;
-    transform: translate(0, -50%);
-    border-radius: 0.25rem;
-    &.btn-next {
-      right: 1rem;
-    }
-    &.btn-prev {
-      left: 1rem;
-    }
-    svg {
-      width: 3rem;
-    }
   }
 `
