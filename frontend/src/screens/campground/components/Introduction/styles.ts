83f65ed223c4b01e73a0dc1b19a3ece5e9324173
Kate <test@gmail.com>
2021-04-12 19:48:55 -0400

    save the draft style of campground page

--- frontend/src/screens/campground/components/Introduction/styles.ts
+++ frontend/src/screens/campground/components/Introduction/styles.ts
@@ -1,7 +1,31 @@
 import styled from 'styled-components'
-import { medium, large } from 'styles/media-queries'
+import { medium, large, xlarge } from 'styles/media-queries'
 
-export const Wrapper = styled.div.attrs(() => ({ className: 'camp-intro' }))``
+export const Wrapper = styled.div.attrs(() => ({ className: 'camp-intro' }))`
+  .imgs-swiper {
+    overflow: hidden;
+  }
+  .swiper-slide {
+    min-height: calc(6.8rem + 10vw);
+    width: 100% !important;
+    background-size: cover;
+    background-position: center;
+  }
+  ${medium} {
+    .swipe-slide {
+      max-height: 12rem;
+    }
+  }
+  ${large} {
+    display: flex;
+    flex-direction: row;
+    .imgs-swiper {
+      width: 45%;
+      max-height: 16rem;
+      margin-right: calc(2rem + 2vw);
+    }
+  }
+`
 
 export const IntroText = styled.div`
   &:nth-child(2n) {
@@ -20,6 +44,16 @@ export const IntroText = styled.div`
     align-items: center;
     justify-content: flex-start;
     margin-bottom: 0.5rem;
+    &.rating-stats {
+      span {
+        margin-right: auto;
+      }
+      strong {
+        margin-left: 1rem;
+        font-size: 80%;
+        flex-grow: 2;
+      }
+    }
   }
   button,
   span {
