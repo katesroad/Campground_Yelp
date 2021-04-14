29165d88297d5bfb1da929c7b7898d3fe9669016
Kate <test@gmail.com>
2021-04-14 00:30:33 -0400

    move query result to camplist

--- frontend/src/screens/campgrounds/index.tsx
+++ frontend/src/screens/campgrounds/index.tsx
@@ -1,21 +1,16 @@
 // eslint-disable-next-line
 import styled from 'styled-components/macro'
 import * as React from 'react'
-import CampList, { Campground } from './components/CampList'
+import CampList from './components/CampList'
 import { useGetCampgrounds } from 'hooks/campgrounds.hooks'
-import { Spinner } from 'components/lib'
 
 export default function CampgroundsScreen() {
-  const { data, status, error } = useGetCampgrounds()
+  const campsQuery = useGetCampgrounds()
   React.useEffect(() => {
     document.title = 'Campgrounds | YelpCamp'
     return () => {
       document.title = 'YelpCamp'
     }
   }, [])
-  return (
-    <CampList camps={data?.data}>
-      {['loading', 'idle'].includes(status) ? <Spinner /> : null}
-    </CampList>
-  )
+  return <CampList query={campsQuery} />
 }
