6ee10fb4b06bf14af6ddb2753a3067a81ef50d2a
Kate <3837437+katesroad@users.noreply.github.com>
2021-04-21 21:43:11 -0400

    using fixture for campground detail page testing (#19)
    * add fixture for testing
    
    * use fixture for campgrounds.authenticated.spec.js
    
    Co-authored-by: Kate <test@gmail.com>

--- frontend/cypress/integration/campgrounds.authenticated.spec.js
+++ frontend/cypress/integration/campgrounds.authenticated.spec.js
@@ -1,18 +1,18 @@
 const baseUrl = 'http://192.168.0.117:3000'
-const account = {
-  email: 'c@c.com',
-  password: 'ccc',
-}
 
 describe('vist campground detail page with authentication', () => {
   // get authenticated
   beforeEach(() => {
-    cy.visit(baseUrl + '/login')
-    cy.get('input[name=email]').type(account.email)
-    cy.get('input[name=password]').type(account.password)
-    cy.get('button[type=submit]').click()
-    // visit a campground page
-    cy.get('.camp-link').first().click()
+    cy.fixture('account.json')
+      .as('account')
+      .then((account) => {
+        cy.visit(baseUrl + '/login')
+        cy.get('input[name=email]').type(account.email)
+        cy.get('input[name=password]').type(account.password)
+        cy.get('button[type=submit]').click()
+        // visit a campground page
+        cy.get('.camp-link').first().click()
+      })
   })
 
   it('Authenticated user can create review', () => {
@@ -22,36 +22,32 @@ describe('vist campground detail page with authentication', () => {
       .then((text) => {
         // get the current count number;
         const ratingCount = text.split(' ')[0]
-        const signature = Math.random(0, 1)
-        const review = {
-          rating: Math.ceil(signature * 4 + 1),
-          title: 'Love this campground' + signature,
-          body:
-            'We spent a week at site 11. The hosts were super nice and helpful.' +
-            signature,
-        }
+        cy.fixture('review.json')
+          .as('review')
+          .then((review) => {
+            // creating review
+            const signature = Math.random(0, 1)
+            cy.get('.btn.btn--review').first().click()
+            cy.get('.alert-buttons .btn-confirm').should('not.exist')
 
-        // creating review
-        cy.get('.btn.btn--review').first().click()
-        cy.get('.alert-buttons .btn-confirm').should('not.exist')
+            cy.get('input[name=title]').type(review.title + signature)
+            cy.get('textarea[name=body]').type(review.body + signature)
+            cy.get(`form label[for=rating-${review.rating}]`).click()
+            cy.get('button[type=submit]').first().click()
 
-        cy.get('input[name=title]').type(review.title)
-        cy.get('textarea[name=body]').type(review.body)
-        cy.get(`form label[for=rating-${review.rating}]`).click()
-        cy.get('button[type=submit]').first().click()
+            // force page to reload the latest reviews
+            cy.reload()
 
-        // force page to reload the latest reviews
-        cy.reload()
-
-        // validate the number and review content
-        cy.get('.review-list .review-title')
-          .first()
-          .should('have.text', review.title)
-        cy.get('.review-list .review-body')
-          .first()
-          .should('have.text', review.body)
-        const ratingCountText = +ratingCount + 1 + ' reviews'
-        cy.get('.rating-count').first().should('have.text', ratingCountText)
+            // validate the number and review content
+            cy.get('.review-list .review-title')
+              .first()
+              .should('have.text', review.title + signature)
+            cy.get('.review-list .review-body')
+              .first()
+              .should('have.text', review.body + signature)
+            const ratingCountText = +ratingCount + 1 + ' reviews'
+            cy.get('.rating-count').first().should('have.text', ratingCountText)
+          })
       })
   })
 
@@ -71,11 +67,10 @@ describe('vist campground detail page with authentication', () => {
       .invoke('text')
       .then((text) => {
         const originalReviews = +text.split(' ')[0]
-        console.log(originalReviews)
         cy.get('.review-list .btn--delete').first().click()
 
         cy.reload()
-        
+
         cy.get('.rating-count')
           .invoke('text')
           .then((text) => {
